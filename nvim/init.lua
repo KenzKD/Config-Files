bd4e05da96e6de5098baf1bf8c98ea3cc65f9105
bobettes1 <kenzelkdsouza@gmail.com>
2024-03-17 19:32:46 +0400

    Added Capital Letters and adjusted position of Next and Previous Word

--- nvim/init.lua
+++ nvim/init.lua
@@ -7,11 +7,17 @@ local keys = {
   n = 'h',
   e = 'j',
   i = 'k',
-  o = 'l'
+  o = 'l',
+
+  w = 't',
+  b = 'r',
+  r = 'b',
+  t = 'w'
 }
 
 for _, mode in ipairs(modes) do
   for key, remap in pairs(keys) do
     vim.api.nvim_set_keymap(mode, key, remap, {noremap = true, silent = true})
+    vim.api.nvim_set_keymap(mode, key:upper(), remap:upper(), {noremap = true, silent = true})
   end
 end
