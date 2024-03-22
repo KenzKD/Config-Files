e8799b2e9c6b80d2073a0cc345f943adf6e594e1
bobettes1 <kenzelkdsouza@gmail.com>
2024-03-22 23:17:04 +0400

    Added more Comments and Removed Text Object Remap since it wasn't working

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,22 +1,22 @@
 local modes = {'n', 'v', 'o' }
 local keys = {
-  b = 'n',
-  j = 'e',
-  k = 'i',
-  w = 'o',
+  b = 'n',  -- Find "next" 
+  j = 'e',  -- End of Word
+  k = 'i',  -- Insert
+  w = 'o',  -- Open Below
 
   n = 'b',  -- Go Previous Word
   e = 'j',  -- Go Up
   i = 'k',  -- Go Down
   o = 'w',  -- Go Next Word
 
-  h = 's', 
-  l = 't',
+  h = 's',  -- substitute char
+  l = 't',  -- until char
   
   s = 'h',  -- Go Left  
   t = 'l',  -- Go Right
 
-  d = 'r',
+  d = 'r',  -- Replace
   r = 'd'   -- Delete
 }
 
@@ -26,12 +26,6 @@ for _, mode in ipairs(modes) do
     vim.api.nvim_set_keymap(mode, key, remap, {noremap = true, silent = true})
     vim.api.nvim_set_keymap(mode, key:upper(), remap:upper(), {noremap = true, silent = true})
   end
-end
-
--- Remap "i" to "k" for Text Objects
-local text_objects = {'w', 's', 'p', '"', '(', ')', '[', ']', '{', '}', 't'}
-for _, object in ipairs(text_objects) do
-  vim.api.nvim_set_keymap('o', 'k' .. object, 'i' .. object, {noremap = true, silent = true})
 end
 
 vim.o.clipboard = "unnamedplus"
