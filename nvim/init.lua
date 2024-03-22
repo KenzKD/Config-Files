5354b679f2e1c5b5864c68324b7ecfe65cae67f2
bobettes1 <kenzelkdsouza@gmail.com>
2024-03-22 16:06:48 +0400

    Reshuffled some keys and Also Remapped Delete key and "i" Text Objects to "k"

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,25 +1,37 @@
-local modes = {'n', 'v'}
+local modes = {'n', 'v', 'o' }
 local keys = {
-  h = 'n',
+  b = 'n',
   j = 'e',
   k = 'i',
-  l = 'o',
-  n = 'h',
-  e = 'j',
-  i = 'k',
-  o = 'l',
+  w = 'o',
 
-  w = 't',
-  b = 'r',
-  r = 'b',
-  t = 'w'
+  n = 'b',  -- Go Previous Word
+  e = 'j',  -- Go Up
+  i = 'k',  -- Go Down
+  o = 'w',  -- Go Next Word
+
+  h = 's', 
+  l = 't',
+  
+  s = 'h',  -- Go Left  
+  t = 'l',  -- Go Right
+
+  d = 'r',
+  r = 'd'   -- Delete
 }
 
+-- Remap Keys
 for _, mode in ipairs(modes) do
   for key, remap in pairs(keys) do
     vim.api.nvim_set_keymap(mode, key, remap, {noremap = true, silent = true})
     vim.api.nvim_set_keymap(mode, key:upper(), remap:upper(), {noremap = true, silent = true})
   end
+end
+
+-- Remap "i" to "k" for Text Objects
+local text_objects = {'w', 's', 'p', '"', '(', ')', '[', ']', '{', '}', 't'}
+for _, object in ipairs(text_objects) do
+  vim.api.nvim_set_keymap('o', 'k' .. object, 'i' .. object, {noremap = true, silent = true})
 end
 
 vim.o.clipboard = "unnamedplus"
